206d07d905bdbbac972319841f6fb1d74d378a7f
JesseEMC <j.wolters@erasmusmc.nl>
2024-08-01 15:52:09 +0200

    Update person.py
    Added birth date

--- deduce/person.py
+++ deduce/person.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Union
+from datetime import datetime
 
 
 @dataclass
@@ -15,6 +16,14 @@ class Person:
     first_names: Optional[list[str]] = None
     initials: Optional[str] = None
     surname: Optional[str] = None
+    birth_date: Optional[Union[datetime, str]] = None
+
+    def __post_init__(self):
+        if isinstance(self.birth_date, str):
+            try:
+                self.birth_date = datetime.strptime(self.birth_date, "%d-%m-%Y")
+            except ValueError:
+                raise ValueError("Birth date format should be DD-MM-YYYY")
 
     @classmethod
     def from_keywords(
@@ -23,6 +32,7 @@ class Person:
         patient_initials: str = "",
         patient_surname: str = "",
         patient_given_name: str = "",
+        patient_birth_date: str = "",
     ) -> Person:
         """
         Get a Person from keywords. Mainly used for compatibility with keyword as used
@@ -50,4 +60,5 @@ class Person:
             first_names=patient_first_names_lst or None,
             initials=patient_initials or None,
             surname=patient_surname or None,
+            birth_date=patient_birth_date or None,
         )
