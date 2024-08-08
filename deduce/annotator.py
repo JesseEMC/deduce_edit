ac4f2858338ae6ac35a865b06b39ab9b5d7ae789
JesseEMC <j.wolters@erasmusmc.nl>
2024-08-08 16:24:09 +0200

    Update annotator.py

--- deduce/annotator.py
+++ deduce/annotator.py
@@ -542,7 +542,7 @@ class BirthDateAnnotator(dd.process.Annotator):
     
     def annotate(self, doc: Document) -> list[Annotation]:
 
-        print(doc.metadata["patient"])
+        print(doc.metadata["patient"].initials)
         annotations = []
 
         for match in self.bd_regexp.finditer(doc.text):
