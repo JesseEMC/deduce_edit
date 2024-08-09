eb97c68de20f2d0ddec08f7908a0e8a5657e10c6
JesseEMC <j.wolters@erasmusmc.nl>
2024-08-09 13:04:14 +0200

    Update annotator.py

--- deduce/annotator.py
+++ deduce/annotator.py
@@ -592,7 +592,7 @@ class BirthDateAnnotator(dd.process.Annotator):
             #digits = re.sub(r"\D", "", text)
             #print(digits)
             print("text:", text)
-            res = self.converter(text)
+            #res = self.converter(text)
             #print("Converted date text:", res)
             
 
